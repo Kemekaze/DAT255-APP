a29b35002974ae719102d86938d16717ba50b1e6
Idomin <sannymitra@gmail.com>
2015-09-22 16:22:44 +0200

    Påbörjat api hanteringen

---
+++ Server/lib/api/api.js
@@ -0,0 +1,15 @@
+
+// user:grp38 pass: 9-yxani3BZ
+// 25b4ab49-8e6d-44b2-b930-2c97594be68a
+//
+//
+//
+
+var userNamePass = "grp38:9-yxani3BZ";
+
+var encodedKey = window.btoa(userNamePass);
+
+
+
+
+
