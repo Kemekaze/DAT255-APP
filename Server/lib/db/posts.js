6039ac283822266e8a711f952cc7230c8f9f5d06
Elias <3lias.pappas@gmail.com>
2015-09-23 10:34:35 +0200

    lägga till och hämta kommentar er för en post tillagt
    i post är dessa tillagt:
    comment.getAll(post_id)
    comment.save(post_id,body,user)

--- Server/lib/db/posts.js
+++ Server/lib/db/posts.js
@@ -9,7 +9,11 @@ var exports = module.exports = {};
 var PostSchema = new Schema({
   body: { type: String},
   user: { type: String},  
-  comments: [{ body: String, date: Date }],
+  comments: [{ 
+  	 			body: String, 
+  	 			user: String,
+  				date: Date 
+  			}],
   time: { type: Date, default: Date.now },
   hidden: { type: Boolean},
   meta: {
@@ -79,53 +83,21 @@ exports.voteDown = function(id,callback){
 	  callback(p);
 	});
 }
-/*
-exports.voteDown = function(id,callback){
-	// Get a collection
-	var collection = db.get().collection(collectionName);
-	//Create Object id for id
-	var o_id = new ObjectID(id);
+exports.comment.save = function(post_id,body,user,callback){
+	var comment ={
+		body: body, 
+  	 	user: user,
+  		date: (new Date).getTime()
+	};
+	PostModel.where({_id:post_id}).update({ $push: { comments: comment }}, function (err, p) {
+	  if (err) return console.error(err);
+	  callback(p);
+	});
 
-	collection.updateOne({_id: o_id},{$inc: {"votes.up":-1}}, function(err, result) {	
-		//Test
-		assert.equal(err, null);
-		assert.equal(1, result.result.n);
-    	console.log("Upvoted post with id: "+id);
-    	//Callback
-	    callback(result);
-	    }
-	);
-}
+} 
+exports.comment.getAll = function(post_id,callback){
+	exports.findById(post_id,function(post){
+		callback(post.comments);
+	})
 
-exports.get = function(key, value, options, callback){
-	// Get a collection
-	var collection = db.get().collection(collectionName);
-	//Create object
-	var find = {};
-	find[key] = (key == '_id')? new ObjectID(value) : value;
-	//Get an object from collection
-	collection.findOne(find, options, function(err, result) {
-		//Test
-		assert.equal(err, null);
-		//callback
-    	callback(result);
-    });
-	
-}
-// Example ()
-exports.getMany = function(query,fields,skip,limit,callback){
-	// Get a collection
-	var collection = db.get().collection(collectionName);
-	//Get all objects from the collection
-	var options = {skip:skip, limit:limit};
-	if(fields.length != 0) options.fields =fields;
-
-	collection.find(query,options).toArray(function(err, result) {
-		//Test
-		assert.equal(err, null);
-		//Callback
-    	callback(result);
-    });
-	
-}
-*/
+} 
