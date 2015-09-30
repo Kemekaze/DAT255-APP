41ce753818f6793280bd18c008faa96b3a950964
Rasmus <rasmus.webs@gmail.com>
2015-10-01 00:18:22 +0200

    Clean

--- Klient/TestDBConnection/app/src/main/java/elias/testdbconnection/Resources/Constants.java
+++
@@ -1,8 +0,0 @@
-package elias.testdbconnection.Resources;
-
-/**
- * Created by elias on 2015-09-25.
- */
-public class Constants {
-    public static final String SERVER_URL = "http://81.232.129.196:3000";
-}
