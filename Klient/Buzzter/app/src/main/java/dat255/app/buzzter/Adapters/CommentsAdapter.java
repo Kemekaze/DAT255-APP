5c8300424bf0ad770b2d7508f6d2f0e8c427a257
William <wilfor@student.chalmers.se>
2015-10-07 21:34:15 +0200

    kommentarfunktion skelett, inte länkat till appen än

---
+++ Klient/Buzzter/app/src/main/java/dat255/app/buzzter/Adapters/CommentsAdapter.java
@@ -0,0 +1,119 @@
+package dat255.app.buzzter.Adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import dat255.app.buzzter.Objects.Post;
+import dat255.app.buzzter.R;
+
+/**
+ * Created by Walle on 2015-10-06.
+ */
+public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.ViewHolder>{
+    private static  final String TAG = "dat255.app.buzzter.CA";
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        // each data item is just a string in this case
+        public TextView body;
+        public TextView user;
+        public TextView time;
+    //    public TextView votes_up;
+    //   public TextView votes_down;
+        public viewHolderClicks viewHolderClicks;
+
+        public ViewHolder(View view, viewHolderClicks listner) {
+            super(view);
+            body = (TextView) view.findViewById(R.id.body);
+            user = (TextView) view.findViewById(R.id.user);
+            time = (TextView) view.findViewById(R.id.time);
+            //votes_up = (TextView) view.findViewById(R.id.votesUp);
+            //votes_down = (TextView) view.findViewById(R.id.votesDown);
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            viewHolderClicks.onclick(v);
+        }
+
+        public static interface viewHolderClicks {
+            public void onclick(View v);
+        }
+    }
+    public List<Post.Comment> comments;
+
+    public CommentsAdapter(List<Post.Comment> comments) { this.comments = comments; }
+
+    public CommentsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                               int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+
+        ViewHolder vh = new ViewHolder(v, new ViewHolder.viewHolderClicks() {
+            @Override
+            public void onclick(View v) {
+                Log.i(TAG, String.valueOf(v.getId()));
+                //Toast.makeText(this.getApplicationContext(), v., Toast.LENGTH_SHORT).show();
+            }
+        });
+        return vh;
+    }
+
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.body.setText(comments.get(position).getBody());
+        holder.user.setText(comments.get(position).getUser());
+        //holder.line.setText(String.valueOf(posts.get(position).getBusLine()));
+        holder.time.setText(comments.get(position).getRelativeTime());
+        // holder.votes_up.setText(String.valueOf(posts.get(position).getVotes()[0]));
+        // holder.votes_down.setText(String.valueOf(posts.get(position).getVotes()[1]));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return comments.size();
+    }
+    public List<Post.Comment> getComments() { return comments; }
+
+    public void addComment(Post.Comment comment) {
+        comments.add(comment);
+        this.notifyDataSetChanged();
+    }
+    public void addCommentBeginning(Post.Comment comment) {
+        comments.add(0, comment);
+        this.notifyDataSetChanged();
+    }
+
+    public void addPosts(List<Post.Comment> comments,int type) {
+        switch(type){
+            case 1:
+                addCommentsRefresh(comments);
+                break;
+            case 2:
+                addCommentsBeginning(comments);
+                break;
+            default:
+                this.comments.addAll(comments);
+                break;
+        }
+        this.notifyDataSetChanged();
+    }
+    private void addCommentsRefresh(List<Post.Comment> comments) {
+        this.comments.addAll(comments);
+        this.notifyDataSetChanged();
+    }
+    private void addCommentsBeginning(List<Post.Comment> comments) {
+        this.comments.addAll(0,comments);
+        this.notifyDataSetChanged();
+    }
+}
---
+++ Klient/Buzzter/app/src/main/java/dat255/app/buzzter/ViewComments.java
@@ -0,0 +1,117 @@
+package dat255.app.buzzter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.LinearLayoutManager;
+
+import java.util.ArrayList;
+
+import dat255.app.buzzter.Adapters.CommentsAdapter;
+import dat255.app.buzzter.Objects.Post;
+
+
+public class ViewComments extends AppCompatActivity {
+    private final String TAG = "dat255.app.buzzter.VC";
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_comments);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.comments_recycler_view);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new CommentsAdapter(new ArrayList<Post.Comment>());
+        mRecyclerView.setAdapter(mAdapter);
+
+      //  mAdapter = new CommentsAdapter() {
+
+      //  }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_view_comment, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void displayComments(){
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
