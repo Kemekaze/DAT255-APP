2287f87ac7f55bd03c12e14f422cd3a9a8454e05
Idomin <sannymitra@gmail.com>
2015-10-19 21:40:41 +0200

    Fixade lite

--- Klient/Busster/app/src/main/java/dat255/busster/AddCommentActivity.java
+++ Klient/Busster/app/src/main/java/dat255/busster/AddCommentActivity.java
@@ -60,6 +60,7 @@ public class AddCommentActivity extends AppCompatActivity {
                 return true;
             case R.id.send_comment_action:
                 saveComment();
+                finish();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
