e574527f5c4301f1934deb99bd1e281b28771aa7
William <wilfor@student.chalmers.se>
2015-10-29 22:39:16 +0100

    kommentarer :))

--- Klient/Busster/app/src/main/java/dat255/busster/AddPostActivity.java
+++ Klient/Busster/app/src/main/java/dat255/busster/AddPostActivity.java
@@ -22,10 +22,23 @@ import dat255.busster.Resources.Notifyer;
 import de.greenrobot.event.EventBus;
 import de.greenrobot.event.Subscribe;
 
-
+/**
+ * This activity is for adding a new post to the feed. It lets the
+ * user input text to a TextInputLayout and when finished packs
+ * the body, user and timeSince data into a JSONObject and
+ * sends it over the DataBus to be sent to the server by
+ * SocketService.
+ */
 public class AddPostActivity extends AppCompatActivity {
     private final String TAG = "dat255.AddPostActivity";
     PreferencesDBHandler preferencesDBHandler;
+
+    /**
+     * Sets content view, sets action bar parameters and
+     * initializes TextInputLayout and adds TextChangedListener
+     * to it.
+     * @param savedInstanceState containing application state.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,26 +57,45 @@ public class AddPostActivity extends AppCompatActivity {
         preferencesDBHandler = new PreferencesDBHandler(this,null);
 
     }
+
+    /**
+     * Registers this activity to the EventBus when started.
+     */
     @Override
     protected void onStart() {
         Log.i(TAG, "onStart");
         EventBus.getDefault().register(this);
         super.onStart();
     }
+
+    /**
+     * Unregisters this activity from the EventBus when stopped.
+     */
     @Override
     protected void onStop() {
         Log.i(TAG,"onStop");
         EventBus.getDefault().unregister(this);
         super.onStop();
     }
+
+    /**
+     * Inflates the menu items for use in the action bar.
+     * @param menu item to be inflated
+     * @return true if successful, false if unsuccessful
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu items for use in the action bar
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.add_post_actions, menu);
 
         return super.onCreateOptionsMenu(menu);
     }
+
+    /**
+     * Performs actions based on a selected MenuItem.
+     * @param item selected MenuItem
+     * @return true of successful, false if unsuccessful.
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -78,6 +110,11 @@ public class AddPostActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    /**
+     * Saves the body of the TextInputLayout together with username and
+     * current time to a JSONObject and posts it to the EventBus.
+     */
     public void savePost() {
         Log.i(TAG,"savePost");
         TextInputLayout body = (TextInputLayout) findViewById(R.id.post_body);
@@ -92,12 +129,14 @@ public class AddPostActivity extends AppCompatActivity {
         }catch (JSONException e) {
             e.printStackTrace();
         }
-
-
-
         preferencesDBHandler.close();
     }
-    //Eventbus events
+
+    /**
+     * Checks SavePostEvent if it was successful and posts
+     * a sticky Event.
+     * @param event
+     */
     @Subscribe
     public void savePostStatus(SavePostEvent event){
         Log.i(TAG, "savePostStatus");
@@ -105,7 +144,7 @@ public class AddPostActivity extends AppCompatActivity {
             EventBus.getDefault().postSticky(new StatusEvent("Post saved!"));
             this.finish();
         }
-        //BEhandla error för fel.
+        // handle errors.
         else Log.e(TAG, "PostSaveError");
     }
 }
